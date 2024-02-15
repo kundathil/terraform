bcfe52bfef88b548a0774a9bdc2a04cf26025065
Nithin Kundathil <knithin.sai@gmail.com>
2024-02-16 00:02:45 +0530

    EC2

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
-resource "aws_instance" "foo" {
+resource "aws_instance" "sairam" {
   ami           = "ami-05fa00d4c63e32376" # us-west-2
   instance_type = "t2.micro"
   tags = {
       Name = "Nithin Kundathil"
   }
+}
